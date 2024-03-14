79c033094099ee158b0ec2b6c45d4f1d94fc96c7
NamrataMukt <143001822+NamrataMukt@users.noreply.github.com>
2024-03-14 10:51:15 +0530

    added crossorigin into the controller

--- Home-Loan-Management-System/src/main/java/in/cjctech/wonderhomeapp/app/controller/EnquiryController.java
+++ Home-Loan-Management-System/src/main/java/in/cjctech/wonderhomeapp/app/controller/EnquiryController.java
@@ -1,15 +1,17 @@
 package in.cjctech.wonderhomeapp.app.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
 import in.cjctech.wonderhomeapp.app.model.Enquiry;
 import in.cjctech.wonderhomeapp.app.service.EnquiryService;
 
+@CrossOrigin("*")
 @RestController
 public class EnquiryController 
 {
